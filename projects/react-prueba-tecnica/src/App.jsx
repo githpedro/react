0c789b97c8b6b7d1664dcac70f2a51f113276c77
Pedro <55480720+pedroklk@users.noreply.github.com>
2024-09-04 18:49:08 +0000

    Prueba Técnica

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react"
+import './App.css'
 
 const gato_random_fact = 'https://catfact.ninja/fact';
 //const gato_endpoint = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+const cat_prefix_image_url = 'https://caatas.com'
+
 export function App () {
     const [fact, setFact] = useState()
     const [imageUrl, setImageurl] = useState()
@@ -12,23 +15,27 @@ export function App () {
             .then(data => { 
                 const {fact} = data
                 setFact(fact)
-            
-                const threefirstWord = fact.split(' ', 3).join(' ')
-
-                fetch(gato_random_fact)
-                    .then(res => res.json())
-                    .then(response => {
-                        const {url} = response
-                    })
             })
     }, [])
+
+    useEffect(() => {
+        if (!fact) return     
+        const threefirstWord = fact.split(' ', 3).join(' ')
+
+        fetch(`https://cataas.com/cat/says/${threefirstWord}?fontSize=50&fontColor=red&json=true`)
+            .then(res => res.json())
+            .then(response => {
+                const {url} = response
+                setImageurl(url)
+            })
+    }, [fact])
 
     return (
         <main>
 
         <h1>App</h1>
         {fact && <p>{fact}</p>}
-        {imageUrl && <img src={imageUrl} alt="cat" />} 
+        {imageUrl && <img src={`${cat_prefix_image_url}${imageUrl}`} alt={`Cat about API of ${fact}`} />} 
         </main>
 
     )
